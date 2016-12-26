12cf9dcf86fbb53b21a10d266997f723eb11accf
netcan <1469709759@qq.com>
2016-12-26 15:05:28 +0800

    初始版本，完成数据库建模

---
+++ README.md
@@ -0,0 +1,2 @@
+## HFUT\_Market
+合肥工业大学数据库课设，Django框架。
---
+++ models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Commodity(models.Model):
+    # 商品信息
+    name = models.CharField(max_length = 50)
+    introduction = models.TextField()
+    image = models.ImageField()
+    available = models.BooleanField(default = True)
+    date = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField(
+        "Tag",
+        db_table = "commodity_tag"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    # 商品分类，一对一
+    name = models.CharField(max_length = 50, unique = True)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    # 商品标签，多对多
+    name = models.CharField(max_length = 50, unique = True)
+    def __str__(self):
+        return self.name
+
